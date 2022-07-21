cbfde9ba8fdd5044bf4afa461895c0a33a1ff9c4
RafaelRufino <rafaelrufino.52@gmail.com>
2022-07-21 00:19:46 -0300

    feta: criando component de home

---
+++ src/screens/Home.tsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import {
+  VStack,
+  HStack,
+  IconButton,
+  useTheme,
+  Text,
+  Heading,
+  FlatList,
+} from "native-base";
+
+import Logo from "../assets/logo_secondary.svg";
+import { SignOut } from "phosphor-react-native";
+import { Filter } from "../components/Filter";
+import { Order, OrderProps } from "../components/Order";
+import { Button } from "../components/Button";
+
+export function Home() {
+  const [statusSelector, setStatusSelector] = useState<"open" | "closed">(
+    "open"
+  );
+  const [orders, setOrders] = useState<OrderProps[]>([
+    {
+      id: "123",
+      patrimony: "123456",
+      when: "18/07/2022 ás 10:00",
+      status: "open",
+    },
+  ]);
+
+  const { colors } = useTheme();
+  return (
+    <VStack flex={1} pb={6} bg="gray.700">
+      <HStack
+        w="full"
+        justifyContent="space-between"
+        alignItems="center"
+        bg="gray.600"
+        pt={12}
+        pb={5}
+        px={6}
+      >
+        <Logo />
+        <IconButton icon={<SignOut size={26} color={colors.green[700]} />} />
+      </HStack>
+
+      <VStack flex={1} px={6}>
+        <HStack
+          w="full"
+          mt={8}
+          mb={4}
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Heading color="gray.100">Meus chamados</Heading>
+          <Text color="gray.200">3</Text>
+        </HStack>
+        <HStack space={3} mb={8}>
+          <Filter
+            type="open"
+            title="em andamento"
+            onPress={() => setStatusSelector("open")}
+            isActive={statusSelector === "open"}
+          />
+          <Filter
+            type="closed"
+            title="Finalizado"
+            onPress={() => setStatusSelector("closed")}
+            isActive={statusSelector === "closed"}
+          />
+        </HStack>
+
+        <FlatList
+          data={orders}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Order data={item} />}
+        />
+        <Button name="Nova Solicitação" />
+      </VStack>
+    </VStack>
+  );
+}
