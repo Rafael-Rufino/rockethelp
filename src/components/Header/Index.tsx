278dfa2027cd5ef9b4706b183bb35025375da377
RafaelRufino <rafaelrufino.52@gmail.com>
2022-07-21 02:36:37 -0300

    feat: criando component de header

---
+++ src/components/Header/Index.tsx
@@ -0,0 +1,51 @@
+import { CaretLeft } from "phosphor-react-native";
+
+import {
+  HStack,
+  IconButton,
+  useTheme,
+  Heading,
+  StyledProps,
+} from "native-base";
+
+import { useNavigation } from "@react-navigation/native";
+
+type IProps = StyledProps & {
+  title: string;
+};
+export function Header({ title, ...rest }: IProps) {
+  const navigation = useNavigation();
+
+  const { colors } = useTheme();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <HStack
+      w="full"
+      justifyContent="space-between"
+      alignItems="center"
+      bg="gray.600"
+      pt={12}
+      pb={6}
+      px={6}
+      {...rest}
+    >
+      <IconButton
+        icon={<CaretLeft size={24} color={colors.green[200]} />}
+        onPress={handleGoBack}
+      />
+      <Heading
+        color="gray.100"
+        textAlign="center"
+        fontSize="lg"
+        flex={1}
+        ml={-6}
+      >
+        {title}
+      </Heading>
+    </HStack>
+  );
+}
