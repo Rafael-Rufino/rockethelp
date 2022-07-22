5a8d50e08046c8f9362353b84055189cf0b43bec
RafaelRufino <rafaelrufino.52@gmail.com>
2022-07-22 17:15:31 -0300

    refactor: finalizando tela de login

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,17 +1,45 @@
 import React, { useState } from "react";
+import auth from "@react-native-firebase/auth";
+
 import { Heading, VStack, Icon, useTheme } from "native-base";
 import { Envelope, Key } from "phosphor-react-native";
 import Logo from "../assets/logo_primary.svg";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
+import { Alert } from "react-native";
 
 export function SignIn() {
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { colors } = useTheme();
 
-  function handleSubmit() {
-    console.log(email, password);
+  function handleSignIn() {
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informe e-mail e senha.");
+    }
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if (error.code === "auth/invalid-email") {
+          return Alert.alert("Entrar", "E-mail inválido.");
+        }
+
+        if (error.code === "auth/wrong-password") {
+          return Alert.alert("Entrar", "E-mail ou senha inválido.");
+        }
+
+        if (error.code === "auth/user-not-found") {
+          return Alert.alert("Entrar", "E-mail ou senha inválido.");
+        }
+
+        return Alert.alert("Entrar", "Não foi possivel acessar.");
+      });
   }
 
   return (
@@ -26,15 +54,21 @@ export function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
-        onChangeText={(text) => setEmail(text)}
+        onChangeText={setEmail}
       />
       <Input
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
-        onChangeText={(text) => setPassword(text)}
+        onChangeText={setPassword}
       />
-      <Button name="Entrar" w="full" mt={8} onPress={handleSubmit} />
+      <Button
+        isLoading={isLoading}
+        name="Entrar"
+        w="full"
+        mt={8}
+        onPress={handleSignIn}
+      />
     </VStack>
   );
 }
