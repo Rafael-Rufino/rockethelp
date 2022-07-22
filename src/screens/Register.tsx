f544faeb25bcf22cb71c33c7d618bbb3143db312
RafaelRufino <rafaelrufino.52@gmail.com>
2022-07-22 17:16:22 -0300

    feat: component de cadastro de serviços

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -5,28 +5,69 @@ import { Input } from "../components/Input";
 import { Header } from "../components/Header/Index";
 
 import { VStack } from "native-base";
+import { Alert } from "react-native";
+
+import firestore from "@react-native-firebase/firestore";
+import { useNavigation } from "@react-navigation/native";
 
 export function Register() {
-  const [title, setTitle] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState("");
   const [description, setDescription] = useState("");
+  const navigation = useNavigation();
 
-  function handleRegister() {
-    console.log(title, description);
+  function handleNewRegister() {
+    if (!patrimony || !description) {
+      return Alert.alert("Register", "Preencha todos os campos.");
+    }
+
+    setIsLoading(true);
+
+    firestore()
+      .collection("orders")
+      .add({
+        patrimony,
+        description,
+        status: "open",
+        created_at: firestore.FieldValue.serverTimestamp(),
+      })
+      .then(() => {
+        Alert.alert("Solicitação", "Solicitação registrada com sucesso.");
+        navigation.goBack();
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+        return Alert.alert(
+          "Solicitação",
+          "Não foi possivel registrar o pedido."
+        );
+      });
   }
   return (
     <VStack flex={1} bg="gray.600" p={6}>
       <Header title="Nova solicitação" />
-      <Input placeholder="Número do patrimônio" onChangeText={setTitle} />
+      <Input
+        placeholder="Número do patrimônio"
+        mt={4}
+        onChangeText={(text) => setPatrimony(text)}
+      />
       <Input
         placeholder="Descrição do problema"
         flex={1}
         mt={5}
         multiline
         textAlignVertical="top"
-        onChangeText={setDescription}
+        onChangeText={(text) => setDescription(text)}
       />
 
-      <Button name="Cadastrar" w="full" mt={5} onPress={handleRegister} />
+      <Button
+        isLoading={isLoading}
+        name="Cadastrar"
+        w="full"
+        mt={5}
+        onPress={handleNewRegister}
+      />
     </VStack>
   );
 }
